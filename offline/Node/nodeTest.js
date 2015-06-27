87d042dbb940d00b75c2355b00336dc7dd1c64c2
Konstantinos Vasilakos <KV@KonstaninossMBP.lan>
2015-06-27 15:47:55 +0300

    order

---
+++ offline/Node/nodeTest.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var jsonObj  = require("/Users/KV/Dev/projects/DarkMatter/jsonfiles/data.json");
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end( JSON.stringify(jsonObj));
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
