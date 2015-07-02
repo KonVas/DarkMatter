082a1f326315647639d96c3e2c377a4421b45e29
Konstantinos Vasilakos <KV@KVMacBookPro.lan>
2015-07-02 21:06:44 +0300

    changes

--- offline/Node/nodeTest.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var jsonObj  = require("/Users/KV/Dev/projects/DarkMatter/offline/jsonfiles/data.json");
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end( JSON.stringify(jsonObj));
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
