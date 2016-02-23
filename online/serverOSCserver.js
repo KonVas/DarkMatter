c9411916399ba7c32956481fee6a11ed86b23d83
Konstantinos <kv.syntaxerror@gmail.com>
2016-02-23 17:57:06 +0200

    Streamer file and new data files

---
+++ online/serverOSCserver.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    socketio = require('socket.io'),
+    osc = require('osc.io'),
+    server = http.createServer(),
+    io = socketio.listen(server);
+
+osc(io);
+server.listen(80);
+
+var oscServer = io.connect('http://localhost/osc/servers/8000'),
+    oscClient = io.connect('http://localhost/osc/clients/8000');
+
+server.on('message', function(message){
+    console.log(message);
+});
+
+setInterval( function(){
+    oscClient.emit('message', ['/osc/test', 200]);
+}, 1000);
+
